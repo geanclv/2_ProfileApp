a4891d8a130a43c7d71bac22cec671cb71be205d
geanclv <geanclv@gmail.com>
2022-05-24 18:41:00 -0500

    Haciendo circular el logo.

--- app/src/main/java/com/geancarloleiva/a2_profileapp/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/a2_profileapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.geancarloleiva.a2_profileapp
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //rounding logo corners
+        val myBitmap = BitmapFactory.decodeResource(resources, R.drawable.logo)
+        val imgRounded = RoundedBitmapDrawableFactory.create(resources, myBitmap)
+        //imgRounded.cornerRadius = 15f
+        imgRounded.isCircular = true
+        //obtaining the logo resource from activity
+        val imgLogo: ImageView = findViewById(R.id.imgLogo)
+        //setting the rounded image
+        imgLogo.setImageDrawable(imgRounded)
     }
 }
