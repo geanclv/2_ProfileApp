0b6d2fccad2cf461587597a7559dc0bdf77d0ddc
geanclv <geanclv@gmail.com>
2022-05-24 14:32:12 -0500

    Initial commit App Kotlin donde se trabajará con diferentes componentes: layout, botones, imágenes, etc.

---
+++ app/src/main/java/com/geancarloleiva/a2_profileapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.geancarloleiva.a2_profileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
